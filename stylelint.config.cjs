c4ccd0f9ad1d7ac50b9d486fbd6e20e84af90cf1
foolish-pine <matsuda.takuya41@gmail.com>
2024-09-22 10:58:36 +0900

    chore: reinstall @wordpress/stylelint-config

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-	extends: [ 'stylelint-config-recess-order' ],
+	extends: [ '@wordpress/stylelint-config', 'stylelint-config-recess-order' ],
 	rules: { 'selector-class-pattern': null },
 	ignoreFiles: [
 		'**/node_modules/**',
