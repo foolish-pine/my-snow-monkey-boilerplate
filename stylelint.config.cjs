6ac5353667a219f0cce330043c3275b5d9ef87f9
foolish-pine <matsuda.takuya41@gmail.com>
2024-09-27 11:13:37 +0900

    chore: update stylelint config

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,9 +1,5 @@
 module.exports = {
 	extends: [ '@wordpress/stylelint-config', 'stylelint-config-recess-order' ],
 	rules: { 'selector-class-pattern': null },
-	ignoreFiles: [
-		'**/node_modules/**',
-		'snow-monkey/**',
-		'my-snow-monkey/**',
-	],
+	ignoreFiles: [ 'node_modules/**', 'vendor/**', 'my-snow-monkey/**' ],
 };
